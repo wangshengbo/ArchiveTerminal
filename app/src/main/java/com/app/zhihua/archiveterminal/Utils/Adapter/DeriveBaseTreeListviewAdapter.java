b832be0166046a3084d684cd7e661c0d630878a5
wangshengbo <604160313@qq.com>
2016-03-07 11:45:00 +0800

    添加CheckBox点击事件

--- app/src/main/java/com/app/zhihua/archiveterminal/Utils/Adapter/DeriveBaseTreeListviewAdapter.java
+++ app/src/main/java/com/app/zhihua/archiveterminal/Utils/Adapter/DeriveBaseTreeListviewAdapter.java
@@ -6,8 +6,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
+import android.widget.CheckBox;
 import android.widget.ListView;
 
+import com.app.zhihua.archiveterminal.R;
 import com.app.zhihua.archiveterminal.Utils.Node;
 import com.app.zhihua.archiveterminal.Utils.TreeHelper;
 
@@ -23,6 +25,7 @@ public abstract class DeriveBaseTreeListviewAdapter<T> extends BaseAdapter {
     protected List<Node> mVisibleNodes;
     protected LayoutInflater mInflater;
     protected ListView mTree;
+    protected CheckBox mCheckBox = null;
 
 //    public interface OnTreeNodeClickListener{
 //        void onClick(Node node, int position);
@@ -41,6 +44,11 @@ public abstract class DeriveBaseTreeListviewAdapter<T> extends BaseAdapter {
         tree.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                mCheckBox = (CheckBox) view.findViewById(R.id.checkbox_derive);
+                mCheckBox.setChecked(!mCheckBox.isChecked());
+                
+
+
 //                expandOrCollapse(position);
 //                if(mListener != null){
 //                    mListener.onClick(mVisibleNodes.get(position),position);
